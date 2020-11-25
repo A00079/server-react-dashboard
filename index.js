1eb79ba65fb14581ca36274c06bd58ac29a43bea
A00079 <bajay0007@gmail.com>
2020-11-25 13:56:18 +0530

    bug fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, 'build')));
-  app.get(/.*/, (req, res) => {
+  app.get("/protal", (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
