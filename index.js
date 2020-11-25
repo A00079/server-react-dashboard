95cf1713c2c43f82c783943f8db6f71c298f5114
A00079 <bajay0007@gmail.com>
2020-11-25 16:52:39 +0530

    app build

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 // If Production Mode On
 if (process.env.NODE_ENV === 'production') {
 
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get(/.*/, (req, res) => {
+  app.use('/',express.static(path.join(__dirname, 'client/build')));
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
   
