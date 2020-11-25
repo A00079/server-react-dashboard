6c9b0cfde3d10b856e394bc89db968cc4fbe32d9
A00079 <bajay0007@gmail.com>
2020-11-25 13:52:37 +0530

    fix in index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, 'build')));
-  app.get("/portal", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
