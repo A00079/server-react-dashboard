ff66b1ed6ed4ec72731ca07d455160aa54bf22f5
A00079 <bajay0007@gmail.com>
2020-11-25 14:04:10 +0530

    app build

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // If Production Mode On
 if (process.env.NODE_ENV === 'production') {
 
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use("/portal",express.static(path.join(__dirname, 'build')));
   app.get("/portal/*", (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
