2811319e4d2f08aa32969b967ca59031c57ab5a2
A00079 <bajay0007@gmail.com>
2020-11-25 16:41:15 +0530

    moved  build

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 // If Production Mode On
 if (process.env.NODE_ENV === 'production') {
 
-  app.use("/portal",express.static(path.join(__dirname, 'build')));
+  app.use("/portal",express.static(path.join(__dirname, 'portal/build')));
   app.get("/portal/*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'portal/build', 'index.html'));
   });
 }
 
