e31a1f205b1d94a66c1305cdbbc15946b8cbe06e
A00079 <bajay0007@gmail.com>
2020-11-25 16:45:03 +0530

    added client folder

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 5000;
 // If Production Mode On
 if (process.env.NODE_ENV === 'production') {
 
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+  
   app.use("/portal",express.static(path.join(__dirname, 'portal/build')));
   app.get("/portal/*", (req, res) => {
     res.sendFile(path.join(__dirname, 'portal/build', 'index.html'));
