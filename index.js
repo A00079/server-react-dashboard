aa41cc15dc045998ae3660dbe6bea15c578b0358
A00079 <bajay0007@gmail.com>
2020-11-25 13:19:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path');
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+// If Production Mode On
+if (process.env.NODE_ENV === 'production') {
+
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
